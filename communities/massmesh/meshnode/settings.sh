14d8203716b112d7164edf936560e1fb0781e62b
Stephen Smith <stephen304@gmail.com>
2019-11-13 00:27:37 -0500

    Refactor: remove duplicated packages and add comments.

--- communities/massmesh/meshnode/settings.sh
+++ communities/massmesh/meshnode/settings.sh
@@ -5,6 +5,10 @@ set -x
 export EXTRA_IMAGE_NAME="massmesh-meshnode"
 
 # Packages for all mesh nodes
-export PACKAGES="${PACKAGES} yggdrasil luci luci-theme-material jq iperf3 nano"
+
+export PACKAGES="${PACKAGES} yggdrasil" # Yggdrasil mesh routing protocol
+export PACKAGES="${PACKAGES} luci luci-theme-material" # Luci for web configuration
+export PACKAGES="${PACKAGES} jq" # jq for scripting jquery config changes
+export PACKAGES="${PACKAGES} iperf3 nano" # Benchmarking / debugging
 
 set +x
