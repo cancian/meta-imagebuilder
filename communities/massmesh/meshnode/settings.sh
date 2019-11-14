7640cf7e56ca66c91572fc4386ca86ed5fec0df3
Stephen Smith <stephen304@gmail.com>
2019-11-14 16:42:18 -0500

    cleanup: Refactor common packages to global massmesh settings

--- communities/massmesh/meshnode/settings.sh
+++ communities/massmesh/meshnode/settings.sh
@@ -7,13 +7,9 @@ export EXTRA_IMAGE_NAME="massmesh-meshnode"
 # Packages for all mesh nodes
 
 export PACKAGES="${PACKAGES} yggdrasil" # Yggdrasil mesh routing protocol
-export PACKAGES="${PACKAGES} luci luci-theme-material" # Luci for web configuration
-export PACKAGES="${PACKAGES} jq" # jq for scripting jquery config changes
-export PACKAGES="${PACKAGES} nano iperf3 tcpdump iputils-ping iputils-ping6 vnstat mtr noping ss" # Benchmarking / debugging
+export PACKAGES="${PACKAGES} jq" # jq for scripting json config changes
 export PACKAGES="${PACKAGES} tor tor-fw-helper tor-resolve torsocks" # Tor
-export PACKAGES="${PACKAGES} haveged" # Ensure entropy
 export PACKAGES="${PACKAGES} nodogsplash" # Captive portal
-export PACKAGES="${PACKAGES} kmod-ath10k-ct ath10k-firmware-qca988x-ct" # QCA wireless firmware for mesh mode
 export PACKAGES="${PACKAGES} kmod-usb-net-rndis" # USB Tethering
 
 set +x
--- communities/massmesh/meshradio/ubnt_nanostation-ac-loco/settings.sh
+++ communities/massmesh/meshradio/ubnt_nanostation-ac-loco/settings.sh
@@ -7,6 +7,4 @@ export VERSION="snapshots"
 export TARGET="ath79/generic"
 export PROFILE="ubnt_nanostation-ac-loco"
 
-export PACKAGES="-wpad-basic tcpdump batctl-full kmod-batman-adv ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-ipt-offload kmod-sched-act-vlan luci wpad-mesh-openssl"
-
 set +x
