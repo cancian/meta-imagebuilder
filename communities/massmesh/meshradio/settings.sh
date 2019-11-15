d54b9ab889acbe28218c3fe8648cd0943f675de8
William Fleurant <william@netblazr.com>
2019-11-15 12:43:31 -0500

    settings: stop bash trace output (remove debug output)

--- communities/massmesh/meshradio/settings.sh
+++ communities/massmesh/meshradio/settings.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-set -x
-
 export EXTRA_IMAGE_NAME="massmesh-meshradio"
 
 export PACKAGES="${PACKAGES} -wpad-basic batctl-full kmod-batman-adv kmod-ipt-offload kmod-sched-act-vlan wpad-mesh-openssl"
-
-set +x
--- communities/massmesh/meshradio/ubnt_nanostation-ac-loco/settings.sh
+++ communities/massmesh/meshradio/ubnt_nanostation-ac-loco/settings.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-set -x
-
 export VERSION="snapshots"
 
 export TARGET="ath79/generic"
 export PROFILE="ubnt_nanostation-ac-loco"
-
-set +x
