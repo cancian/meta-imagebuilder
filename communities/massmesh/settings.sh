ca69018153fcdf1b9fa40c60400676ab7243653c
William Fleurant <meshnet@protonmail.com>
2019-11-18 17:45:25 -0500

    settings: stop bash trace output (remove debug output) (#49)

--- communities/massmesh/settings.sh
+++ communities/massmesh/settings.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-set -x
-
 export PACKAGES="${PACKAGES} luci luci-theme-material" # Luci for web configuration
 export PACKAGES="${PACKAGES} haveged" # Ensure entropy
 export PACKAGES="${PACKAGES} kmod-ath10k-ct ath10k-firmware-qca988x-ct" # QCA wireless firmware for mesh mode
 export PACKAGES="${PACKAGES} nano iperf3 tcpdump iputils-ping iputils-ping6 vnstat mtr noping ss" # Benchmarking / debugging
-
-set +x
