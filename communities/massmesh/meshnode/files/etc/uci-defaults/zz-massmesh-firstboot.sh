b441ff3e8e0c88d37d1389b8ab805bd66ae86b4f
Stephen Smith <stephen304@gmail.com>
2019-11-20 18:34:20 -0500

    firstboot: Enable tunnel routing and add ipv4localsubnets on first boot

--- communities/massmesh/meshnode/files/etc/uci-defaults/zz-massmesh-firstboot.sh
+++ communities/massmesh/meshnode/files/etc/uci-defaults/zz-massmesh-firstboot.sh
@@ -6,8 +6,12 @@ uci commit
 
 # Add yggdrasil peers
 tmp=$(mktemp)
-jq '.Peers += ["tcp://50.236.201.218:56088"]' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
+jq '.Peers = ["tcp://50.236.201.218:56088"]' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
 jq '.Peers += ["tcp://45.76.166.128:12345"]' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
 jq '.Peers += ["tcp://45.77.107.150:34660"]' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
 jq '.Peers += ["tcp://108.175.10.127:61216"]' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
 jq '.Peers += ["tcp://198.58.100.240:44478"]' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
+
+# Add yggdrasil tunnel routing config
+jq '.TunnelRouting.Enable = true' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
+jq '.TunnelRouting.IPv4LocalSubnets = ["0.0.0.0/0"]' /etc/yggdrasil.conf > "$tmp" && mv "$tmp" /etc/yggdrasil.conf
