3939eb5af358c23a1f0f3100de4f14368476032c
William Fleurant <william@netblazr.com>
2020-01-02 17:21:11 -0500

    massmesh: rm ath10k-ct-smallbuffers (fixes ubnt_nanosttaion-ac-loco build)

--- communities/massmesh/settings.sh
+++ communities/massmesh/settings.sh
@@ -2,5 +2,5 @@
 
 export PACKAGES="${PACKAGES} luci luci-theme-material" # Luci for web configuration
 export PACKAGES="${PACKAGES} haveged" # Ensure entropy
-export PACKAGES="${PACKAGES} kmod-ath10k-ct ath10k-firmware-qca988x-ct" # QCA wireless firmware for mesh mode
+export PACKAGES="${PACKAGES} kmod-ath10k-ct ath10k-firmware-qca988x-ct -kmod-ath10k-ct-smallbuffers" # QCA wireless firmware for mesh mode
 export PACKAGES="${PACKAGES} nano iperf3 tcpdump iputils-ping vnstat mtr noping ss" # Benchmarking / debugging
