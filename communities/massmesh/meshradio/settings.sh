b4d6f030dcff465eca954e190b226392adba8af5
William Fleurant <william@netblazr.com>
2020-04-01 11:06:40 -0400

    meshradio: slim fw size down by cutting non-essential packages

--- communities/massmesh/meshradio/settings.sh
+++ communities/massmesh/meshradio/settings.sh
@@ -2,4 +2,4 @@
 
 export EXTRA_IMAGE_NAME="massmesh-meshradio"
 
-export PACKAGES="${PACKAGES} -wpad-basic batctl-full kmod-batman-adv kmod-ipt-offload kmod-sched-act-vlan wpad-mesh-openssl"
+export PACKAGES="${PACKAGES} -wpad-basic batctl-full kmod-batman-adv "
