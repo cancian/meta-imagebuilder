febde2bbb60fb2507a74c73ec9919a170f2fe651
JamesVorder <james@bip44.email>
2020-05-19 21:01:53 -0400

    Remove old python packages to make room for new.

--- communities/massmesh/settings.sh
+++ communities/massmesh/settings.sh
@@ -3,5 +3,5 @@
 export PACKAGES="${PACKAGES} luci luci-theme-material" # Luci for web configuration
 export PACKAGES="${PACKAGES} haveged" # Ensure entropy
 export PACKAGES="${PACKAGES} kmod-ath10k-ct ath10k-firmware-qca988x-ct -kmod-ath10k-ct-smallbuffers" # QCA wireless firmware for mesh mode
-export PACKAGES="${PACKAGES} nano iperf3 curl tcpdump iputils-ping vnstat mtr noping ss" # Benchmarking / debugging
+export PACKAGES="${PACKAGES} nano iperf3 curl tcpdump iputils-ping vnstat mtr noping " # ss" # Benchmarking / debugging
 
